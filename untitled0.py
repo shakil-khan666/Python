e5c38ade43529e38e616445b1f3186b9b06d69be
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2024-05-15 16:36:52 +0600

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,17 @@
+
+
+number1=float(input("enter the  number1: "))
+number2=float(input("enter the number2: "))
+
+result=number1+number2
+print(" the final sum ",result)
+
+result2=number1-number2
+print(" the final sub ",result2)
+
+area=3.1416*number1*number1
+print("the area = ",area)
+
+area2=number1*number1
+print("the borgoketro area ",area2)
+
