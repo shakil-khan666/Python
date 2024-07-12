c537ff023ab4e9c715c4de0cb33215f59770f6d2
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2024-07-12 23:41:24 +0600

    Add files via upload

---
+++ [13]string_method.py
@@ -0,0 +1,10 @@
+course_name = " Cloud computer "
+
+print(course_name.upper())
+print(course_name.lower())
+print(course_name.title())
+print(course_name.strip())
+print(course_name.replace("C"," e"))
+print("pro" in course_name)
+print("shakil" not in course_name)
+
