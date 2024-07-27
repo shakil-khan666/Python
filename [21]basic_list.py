cdab0a05dccac6ec5906f3f71b7a56f9fa162011
shakil-khan666 <96341854+shakil-khan666@users.noreply.github.com>
2024-07-27 19:30:14 +0600

    Add files via upload

---
+++ [21]basic_list.py
@@ -0,0 +1,17 @@
+latters = ["a","b","c","d"]
+print(latters)
+
+martix = [ [3,4],[4,6],[7,9],[10,11]]
+print(martix)
+    
+zero = [0]*5
+print(zero)
+number = zero+martix
+
+print(number)
+
+name = list("hello shakil")
+
+print(name)
+print(len(name))
+print(name[0])
