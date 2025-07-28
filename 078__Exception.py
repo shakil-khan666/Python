cda6899c9a0673f473fb689a72261b8a1b8f6c16
Md shakil khan <96341854+shakil-khan666@users.noreply.github.com>
2025-07-29 01:00:27 +0600

    Add files via upload

---
+++ 078__Exception.py
@@ -0,0 +1,9 @@
+try:
+    a = int(input("enter the number a : "))
+    b = int(input("enter the number b : "))
+    result = a+b
+
+    print("sum = ",result)
+    
+except:
+    print("again input daw")
