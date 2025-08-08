7c712ea3529b52e16f3e10047e99f7a0e7a9c733
Md shakil khan <96341854+shakil-khan666@users.noreply.github.com>
2025-08-08 22:56:41 +0600

    Add files via upload

---
+++ 079_Linked_list.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head=None
+    
+    def append(self,data):
+        new_node=Node(data)
+        if not  self.head:
+            self.head=new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp= temp.next
+        temp.next=new_node
+
+    def Display(self):
+        temp = self.head
+        while temp:
+            print(temp.data,end='->')
+            temp=temp.next
+        print("None")
+
+
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
+
+print("লিস্টের ডেটা:")
+ll.Display()
+
+
+
+
